f979812b899dd0b59d74f8439fa39b334eb64c30
molnia_grom <mgrom250@gmail.com>
2024-11-26 17:20:13 +0200

    I'm starting to make a social network, lesson 12

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://illustrators.ru/uploads/illustration/image/1682866/%D0%B2%D0%BD%D1%82.jpg"></img>
+    </header>
+  )
+}
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const Profile = () => {
+  return (
+    <div className="content">
+    <div>
+      <img src="https://linagro.tn/images/entries/700x300/southern-alps-700x300.png"></img>
+    </div>
+
+    <div>
+      ava + description
+      {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1ZGW2MjMvckMP-lGvROqZ6IEa_mL1soMWDQ&s"></img> */}
+    </div>
+
+    <div>
+      My post
+      <div>New post</div>
+      <div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+    </div>
+  </div>
+  )
+}
