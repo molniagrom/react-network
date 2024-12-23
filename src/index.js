7f280462f50a229c87f61b84e2df608f6289c08b
molnia_grom <mgrom250@gmail.com>
2024-12-23 19:19:40 +0200

    slight adjustment 2

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
-
 // addPost("Samurai")
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
