9b364a02735b715bb56f41227b2c5c9a3aa75771
molnia_grom <mgrom250@gmail.com>
2025-01-20 21:11:57 +0200

    Now I wrote the tests correctly

--- src/redux/test/profile-reduser.test.tsx
+++ src/redux/test/profile-reduser.test.tsx
@@ -5,7 +5,9 @@ test('ADD_MESSAGE adds a new post', () => {
     const newState = profileReducer(initialState, action);
 
     expect(newState.posts.length).toBe(initialState.posts.length + 1);
-    expect(newState.posts[newState.posts.length - 1].post).toBe(initialState.newPostText);
+    expect(newState.posts[0].message).toBe(initialState.newPostText);
     expect(newState.newPostText).toBe('');
 });
+
+
 
