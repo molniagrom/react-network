757381b208af163a572c23b3ccfabd5ffc8831c6
molnia_grom <mgrom250@gmail.com>
2025-01-29 19:22:25 +0200

    I'm trying to bypass CORS so that Users from the server appear on GitHub Pages

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,21 +6,28 @@ import userPhoto from "../../assets/images/user.jpg"
 export class Users extends React.Component {
 
     componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {
+            withCredentials: true
+        })
             .then((response) => {
                 this.props.setUsers(response.data.items);
                 this.props.setTotalUsersCount(response.data.totalCount);
             })
+            .catch(error => console.error("Ошибка загрузки пользователей:", error));
     }
 
     onPageChanged = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber)
+        this.props.setCurrentPage(pageNumber);
 
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {
+            withCredentials: true
+        })
             .then((response) => {
                 this.props.setUsers(response.data.items);
             })
+            .catch(error => console.error("Ошибка загрузки пользователей:", error));
     }
+
 
     render() {
 
