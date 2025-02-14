be699d8d60ffd6fd8f15845645874dab8317d33d
molnia_grom <mgrom250@gmail.com>
2025-02-14 12:59:33 +0200

    add applyMiddleware

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,10 +1,13 @@
 // Импортируем configureStore из @reduxjs/toolkit вместо createStore
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {configureStore, combineReducers, applyMiddleware} from "@reduxjs/toolkit";
 import { profileReducer } from "./profile-reduser";
 import { dialogReducer } from "./dialogs-reduser";
 import { sidebarReducer } from "./sidebar-reduser";
 import {usersReducer} from "./users-reduser";
 import {authReducer} from "./auth-reducer";
+import {thunk} from "redux-thunk";
+
+const thunkMiddleware = thunk;
 
 // Объединяем редьюсеры
 let reducers = combineReducers({
@@ -18,6 +21,7 @@ let reducers = combineReducers({
 // Создаём store с использованием configureStore
 export let store = configureStore({
     reducer: reducers, // Передаём объединённый редьюсер
+    applyMiddleware: applyMiddleware(thunkMiddleware),
 });
 
 // Экспортируем store как default для удобства
