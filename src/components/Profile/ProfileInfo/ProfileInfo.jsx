dafb8a2c00f634fa451b60aa5d4214f8ebb14690
molnia_grom <mgrom250@gmail.com>
2025-02-18 14:18:09 +0200

    I looked 71 - React JS - setState, local state

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import s from "./ProfileInfo.module.css";
 import {Preloader} from "../../common/Preloader/Preloader";
+import {ProfileStatus} from "./ProfileStatus";
 
 export const ProfileInfo = (props) => {
 // debugger
@@ -9,9 +10,10 @@ export const ProfileInfo = (props) => {
     }
     return (
         <div>
-            <div>
-                <img src="https://linagro.tn/images/entries/700x300/southern-alps-700x300.png"></img>
-            </div>
+            {/*<div>*/}
+            {/*    <img src="https://linagro.tn/images/entries/700x300/southern-alps-700x300.png"></img>*/}
+            {/*</div>*/}
+            <ProfileStatus status={"hello"} />
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large} alt="Avatar" className={s.avatar} />
                 <div className={s.info}>
